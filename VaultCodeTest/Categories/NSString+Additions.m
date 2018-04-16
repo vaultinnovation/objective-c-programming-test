94ee97f6f62cc4afaaa2e91695283f75cc30f230
Aaron Crabtree <ac@tapdezign.com>
2018-04-16 13:39:20 -0700

    Updated NSString category
    Finished the implementations of the NSString category string reversal methods

--- VaultCodeTest/Categories/NSString+Additions.m
+++ VaultCodeTest/Categories/NSString+Additions.m
@@ -11,6 +11,14 @@
 @implementation NSString (Additions)
 
 
+/**
+ Creates a new NSString that has been reversed.
+ 
+ If the original string is less than 2 charaters in length the original string is returned.
+ 
+ @return Creates a new NSString from the original string with the characters in reverse order.
+ */
+
 - (NSString *)reverse {
     
     // if string has one character return original string
@@ -29,32 +37,70 @@
     }
     
     return reversedString;
-
 }
+
+
+
+/**
+ Creates a new NSString that has been reversed.
+
+ This method always returns a new copy of the original string.
+
+ @return Creates a new NSString from the original string with the characters in reverse order.
+ */
 
 - (NSString *)copyAndReverse {
     
     // create a copy of the string
+    NSString *input = [NSString stringWithString: self];
+    int length = (int)input.length;
     
     // if the string has one character return a copy of the original string
+    if (length < 2) {
+        return input;
+    }
     
     // create a mutable string the size of the original string
+    NSMutableString *reversedString = [NSMutableString stringWithCapacity: length];
+    for(int i = (length - 1); i >= 0; i--) {
+        NSRange range = NSMakeRange(i, 1);
+        NSString *substring = [input substringWithRange: range];
+        [reversedString appendString: substring];
+    }
     
-    // loop over the range of the string and create a new string that is reversed
-    
-    
-    return nil;
+    return reversedString;
 }
+
+
+
+/**
+ Creates a new NSString that has been reversed.
+ 
+ If the original string is less than 2 charaters in length the original string is returned.
+ 
+ @return Creates a new NSString from the original string with the characters in reverse order.
+ */
 
 - (NSString *)reverseByEnumerating {
     
     // if string has one character return original string
+    NSUInteger length = self.length;
+    if (length < 2) {
+        return self;
+    }
     
     // create a mutable string the size of the original string
+    NSMutableString *reversedString = [[NSMutableString alloc] initWithCapacity: length];
+    NSRange range = NSMakeRange(0, length);
     
-    // enumerate the string and create a new string that is reversed
+    // enumerate the original string and create a new string that is reversed
+    [self enumerateSubstringsInRange:range
+                             options:(NSStringEnumerationReverse | NSStringEnumerationByComposedCharacterSequences)
+                          usingBlock:^(NSString * _Nullable substring, NSRange substringRange, NSRange enclosingRange, BOOL * _Nonnull stop) {
+                              [reversedString appendString:substring];
+                          }];
     
-    return nil;
+    return reversedString;
 }
 
 @end
